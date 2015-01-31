c54812a31c0f9d1b58dd7310b1a14d63ee7f7011
borisr <boris.rabin03@hotmail.com>
2015-01-31 11:14:01 -0500

    Sat31

--- application/controllers/ipgsd.php
+++ application/controllers/ipgsd.php
@@ -32,6 +32,7 @@ class Ipgsd extends CI_Controller {
 
     public function index() {
 	    $this->session->unset_userdata('acct');     //unset account8 becouse of pegenation 
+	     $this->session->unset_userdata('report');     //unset report becouse of pegenation 
 		if (file_exists('./reports/csv_report.csv')){ unlink('./reports/csv_report.csv');}   //delete csv_report.csv report file before new one is created
 		$this->session->unset_userdata('from_date');
        		$this->session->unset_userdata('to_date');
@@ -45,6 +46,7 @@ class Ipgsd extends CI_Controller {
 	{
 $from_date = $_POST['from_month']."/".$_POST['from_day']."/".$_POST['from_year'];
 $to_date = $_POST['to_month']."/".$_POST['to_day']."/".$_POST['to_year'];
+
 // $this->load->database('');
 /*
 $accnt       = $_POST['account_num'];
@@ -57,18 +59,22 @@ $to_year     = $_POST['to_year'];
 $report      = $_POST['report'];
 */
 
-/* set settion for account number */
+/* set sessionsion for pegenations account , reports , dates and any other data */
       if (!empty($this->input->post('account_num'))){
       $this->session->set_userdata('acct', $_POST['account_num']);
-                                                    }
+                                                   }
+      if ($from_date != "//"){
+      $this->session->set_userdata('from_date', $from_date);
+      $this->session->set_userdata('to_date', $to_date);
+                             }
+       if (!empty($_POST['report'])){
+      $this->session->set_userdata('report', $_POST['report']);
+                                                   }
+
+/*--------------------------------------------------------------------------------*/
 
 
-if ($from_date != "//")
-{
-  $this->session->set_userdata('from_date', $from_date);
- $this->session->set_userdata('to_date', $to_date);
-}
-
+$data["report"] = $_POST['report'];
 $from_m = $this->session->userdata('from_date');
 $to_m = $this->session->userdata('to_date');
 
@@ -90,33 +96,16 @@ $sql = "STR_TO_DATE(`Report Date`,  '%d/%m/%Y' ) BETWEEN  STR_TO_DATE('$from_m',
 
 
 /* --- cash flow report ---NUM ROWS */
-$this->db->where("(Blotter_Code='AC' AND Trans_Code='DCD' OR Trans_Code='DCW')", NULL, FALSE);
 
-
-
-
-/*
-$where = "Blotter_Code=AC AND Trans_Code='DCD' OR Trans_Code='DCW'";
-$this->db->where($where);
-
-
-$where_1 = "Blotter_Code='FF' AND Trans_Code='DWT' OR status='DDP'";
-$this->db->or_where($where_1); 
-
-$where_2 = "Blotter_Code='RF' AND Trans_Code='DCW'";
-$this->db->or_where($where_2); 
-
-$where_3 = "Blotter_Code='CC' AND Trade Entry Field!='DCD'";
-$this->db->or_where($where_3); 
-
-
-*/
-
-
-
-
-
-
+if ($this->session->userdata('report') == 1) {
+$this->db->where("(Blotter_Code='AC' AND (Trans_Code='DCD' OR Trans_Code='DCW') OR (Blotter_Code='FF' AND (Trans_Code='DWT' OR Trans_Code='DDP')) 
+OR (Blotter_Code='RF' AND (Trans_Code='DCW')) OR (Blotter_Code='CC' AND (Trade_Entry_Field!='DCW')) 
+OR (Blotter_Code='#M') OR (Blotter_Code='#K') OR (Blotter_Code='$D') OR (Blotter_Code='A+') OR (Blotter_Code='AC') OR (Blotter_Code='AM') OR (Blotter_Code='BZ') OR (Blotter_Code='CZ') OR (Blotter_Code='DZ')
+OR (Blotter_Code='FY') OR (Blotter_Code='FU') OR (Blotter_Code='H}') OR (Blotter_Code='HA') OR (Blotter_Code='JP')
+OR (Blotter_Code='KA') OR (Blotter_Code='K!') OR (Blotter_Code='L-') OR (Blotter_Code='L#') OR (Blotter_Code='LE') OR (Blotter_Code='N8')OR (Blotter_Code='Q,')OR (Blotter_Code='U*')
+OR (Blotter_Code='W!') OR (Blotter_Code='W@') OR (Blotter_Code='X/') OR (Blotter_Code='Z4') OR (Blotter_Code='ZA') OR (Blotter_Code='ZB') OR (Blotter_Code='ZC')
+OR (Blotter_Code='ZD') OR (Blotter_Code='ZQ')  
+)", NULL, FALSE); }
 
 
 
@@ -151,14 +140,15 @@ $sql = "STR_TO_DATE(`Report Date`,  '%d/%m/%Y' ) BETWEEN  STR_TO_DATE('$from_m',
      $this->db->where($sql);
      
 /* --- cash flow report ---BY DAY  */
-$this->db->where("(Blotter_Code='AC' AND Trans_Code='DCD' OR Trans_Code='DCW')", NULL, FALSE);
-
-
-
-
-
-
-
+if ($this->session->userdata('report') == 1) {
+$this->db->where("(Blotter_Code='AC' AND (Trans_Code='DCD' OR Trans_Code='DCW') OR (Blotter_Code='FF' AND (Trans_Code='DWT' OR Trans_Code='DDP')) 
+OR (Blotter_Code='RF' AND (Trans_Code='DCW')) OR (Blotter_Code='CC' AND (Trade_Entry_Field!='DCW')) 
+OR (Blotter_Code='#M') OR (Blotter_Code='#K') OR (Blotter_Code='$D') OR (Blotter_Code='A+') OR (Blotter_Code='AC') OR (Blotter_Code='AM') OR (Blotter_Code='BZ') OR (Blotter_Code='CZ') OR (Blotter_Code='DZ')
+OR (Blotter_Code='FY') OR (Blotter_Code='FU') OR (Blotter_Code='H}') OR (Blotter_Code='HA') OR (Blotter_Code='JP')
+OR (Blotter_Code='KA') OR (Blotter_Code='K!') OR (Blotter_Code='L-') OR (Blotter_Code='L#') OR (Blotter_Code='LE') OR (Blotter_Code='N8')OR (Blotter_Code='Q,')OR (Blotter_Code='U*')
+OR (Blotter_Code='W!') OR (Blotter_Code='W@') OR (Blotter_Code='X/') OR (Blotter_Code='Z4') OR (Blotter_Code='ZA') OR (Blotter_Code='ZB') OR (Blotter_Code='ZC')
+OR (Blotter_Code='ZD') OR (Blotter_Code='ZQ')  
+)", NULL, FALSE); }
 
 $data['q'] =  $this->db->get_where('transactions_archive', array('Account8' => $this->session->userdata('acct')),$config['per_page'], $this->uri->segment(3));
 
@@ -170,7 +160,16 @@ $sql = "STR_TO_DATE(`Report Date`,  '%d/%m/%Y' ) BETWEEN  STR_TO_DATE('$from_m',
      $this->db->where($sql);
      
 /* --- cash flow report ---FOR CSV REPORT */
-$this->db->where("(Blotter_Code='AC' AND Trans_Code='DCD' OR Trans_Code='DCW')", NULL, FALSE);
+
+if ($_POST['report'] == 1) {
+$this->db->where("(Blotter_Code='AC' AND (Trans_Code='DCD' OR Trans_Code='DCW') OR (Blotter_Code='FF' AND (Trans_Code='DWT' OR Trans_Code='DDP')) 
+OR (Blotter_Code='RF' AND (Trans_Code='DCW')) OR (Blotter_Code='CC' AND (Trade_Entry_Field!='DCW')) 
+OR (Blotter_Code='#M') OR (Blotter_Code='#K') OR (Blotter_Code='$D') OR (Blotter_Code='A+') OR (Blotter_Code='AC') OR (Blotter_Code='AM') OR (Blotter_Code='BZ') OR (Blotter_Code='CZ') OR (Blotter_Code='DZ')
+OR (Blotter_Code='FY') OR (Blotter_Code='FU') OR (Blotter_Code='H}') OR (Blotter_Code='HA') OR (Blotter_Code='JP')
+OR (Blotter_Code='KA') OR (Blotter_Code='K!') OR (Blotter_Code='L-') OR (Blotter_Code='L#') OR (Blotter_Code='LE') OR (Blotter_Code='N8')OR (Blotter_Code='Q,')OR (Blotter_Code='U*')
+OR (Blotter_Code='W!') OR (Blotter_Code='W@') OR (Blotter_Code='X/') OR (Blotter_Code='Z4') OR (Blotter_Code='ZA') OR (Blotter_Code='ZB') OR (Blotter_Code='ZC')
+OR (Blotter_Code='ZD') OR (Blotter_Code='ZQ')  
+)", NULL, FALSE); }
 
 
 
